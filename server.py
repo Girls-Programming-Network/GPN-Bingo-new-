a02d431a1ca904695089e30d86c6214f3dbf56f6
Renee Noble <renee.noble@hotmail.com>
2020-06-22 21:01:47 +1000

    tasks come from template file

--- server.py
+++ server.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/bingo')
 def bingo():
-    return render_template('bingo.html')
+    tasks = []
+    with open("tasks.txt") as f:
+        for num, line in enumerate(f):
+            tasks.append({"num": str(num), "title": line.strip()})
+    return render_template('bingo_template.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
