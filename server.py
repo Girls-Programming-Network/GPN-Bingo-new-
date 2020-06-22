c427bfca01a16df096afb31ad1cac7c56b8e2523
Renee Noble <renee.noble@hotmail.com>
2020-06-22 20:23:18 +1000

    Server bugs

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/bingo')
 def bingo():
-    return render_template('index.html')
+    return render_template('bingo.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
